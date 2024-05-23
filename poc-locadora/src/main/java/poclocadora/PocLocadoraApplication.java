69b36dedd83a9f4d46e55034f8a8320a89eff73d
Aquiles Valerio <99809205+AquilesValerio@users.noreply.github.com>
2024-05-23 20:58:44 -0300

    first commit

---
+++ poc-locadora/src/main/java/poclocadora/PocLocadoraApplication.java
@@ -0,0 +1,13 @@
+package poclocadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocLocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLocadoraApplication.class, args);
+	}
+
+}
